63ceba4d7c02c408044a8f475e676d09f3423b87
ryo hang <ryo@webs.com>
2011-05-18 14:51:47 -0400

    add signup new feature into webs.com

--- java/features/webs-signup.feature
+++ java/features/webs-signup.feature
@@ -3,7 +3,7 @@ Feature: User will attempt to signup websid with his email
          Attempt to create a new account when only entering an Email Address
          Test entering in an email address that has an invalid domain
          Test entering in a password that is longer than 256 characters
-         Test entering in a valid email address and a valid password.
+         Test entering in a valid email address and a valid password
                  
 Scenario: This test case will attempt to create a new account without entering any information
 	Given an url "http://www.webs.com"
@@ -30,3 +30,62 @@ Scenario: Test entering in an email address that has an invalid doman
 	When I click on the green Get Started button
 	Then I should be taken back to the home page
 	And the fields should be blank
+	
+Scenario: Test entering in a valid email address and a valid password
+	Given an url "http://www.webs.com"
+	Then the following fields should appear on the home page "Email Address","Password",option "Site Type"
+	When I enter in an "automationtest@webs.com"
+	And I enter password "asdfasdf"
+	When I click on the green Get Started button
+	Then I should be taken to the "Sign Up" page
+	And I should be logged into the email address I just entered
+	And I should see a message "" bubble at the top
+
+Scenario: Email Signup Click on the green Get Started button selecting a Website Type
+	Given an url "http://www.webs.com"
+	Then the following fields should appear on the home page "Email Address","Password",option "Site Type"
+	When I enter in an "automationtest@webs.com"
+	And I enter password "asdfasdf"
+	And I select "Business" as Website type
+	When I click on the green Get Started button
+	Then I should be taken to the "Sign Up" page
+	And I should see "Small Business/Prof." highlighted in Site Category
+	And I close the browser instant
+	Given an url "http://www.webs.com"
+	Then the following fields should appear on the home page "Email Address","Password",option "Site Type"
+	When I enter in an "automationtest@webs.com"
+	And I enter password "asdfasdf"
+	And I select "Group/Organization" as Website type
+	When I click on the green Get Started button
+	Then I should be taken to the "Sign Up" page
+	And I should see "Groups/Orgs" highlighted in Site Category
+	And I close the browser instant
+	Given an url "http://www.webs.com"
+	Then the following fields should appear on the home page "Email Address","Password",option "Site Type"
+	When I enter in an "automationtest@webs.com"
+	And I enter password "asdfasdf"
+	And I select "Personal" as Website type
+	When I click on the green Get Started button
+	Then I should be taken to the "Sign Up" page
+	And I should see "Personal" highlighted in Site Category
+	
+Scenario: Click on the green Get Started button selecting a Website Type
+	Given an url "http://www.webs.com" and opt to just website type
+	And I select "Business" as Website type
+	When I click on the green Get Started button for original signup
+	Then I should be taken to the "Sign Up" page
+	And I should see "Small Business/Prof." highlighted in Site Category
+	And I close the browser instant
+	Given an url "http://www.webs.com" and opt to just website type
+	And I select "Group/Organization" as Website type
+	When I click on the green Get Started button for original signup
+	Then I should be taken to the "Sign Up" page
+	And I should see "Groups/Orgs" highlighted in Site Category
+	And I close the browser instant
+	Given an url "http://www.webs.com" and opt to just website type
+	And I select "Personal" as Website type
+	When I click on the green Get Started button for original signup
+	Then I should be taken to the "Sign Up" page
+	And I should see "Personal" highlighted in Site Category
+	
+	
--- java/src/test/java/com/webs/signup/EmailSignupsteps.java
+++ java/src/test/java/com/webs/signup/EmailSignupsteps.java
@@ -9,6 +9,7 @@ import cuke4duke.annotation.Before;
 import cuke4duke.annotation.I18n.EN.When;
 import cuke4duke.annotation.I18n.EN.Given;
 import cuke4duke.annotation.I18n.EN.Then;
+import cuke4duke.annotation.Pending;
 
 public class EmailSignupsteps {
 	public static String seleniumHost;
@@ -16,6 +17,7 @@ public class EmailSignupsteps {
 	public static String browser;
 	public static String webSite;
 	public String currenturl;
+	private String enteredEmail;
 	
     @Before
     public void setUpClass() throws Exception {
@@ -56,6 +58,27 @@ public class EmailSignupsteps {
 		session().windowMaximize();*/
 	 }
 	
+	 @Given("^an url \"([^\"]*)\" and opt to just website type$")
+	  public void openurlOptToWebsiteType(String url) {
+		 this.currenturl=url;
+			int i = 0;
+			//ryo is going to take chance...new signup page will adhoc display three mode 30%/30%/30% where is another 10%...
+			while (i<10){
+				++i;
+				startSeleniumSession(seleniumHost,seleniumPort,browser,url);
+				session().setTimeout("120000");
+				session().open("/");
+				session().waitForPageToLoad("30000");
+				session().getEval("window.moveTo(-0,-1);");
+				session().windowMaximize();
+				if (session().isElementPresent("//div[contains(@id,'get_started_block') and contains(@style,'display: block')]")){
+					break;				
+				}
+				session().stop();
+			}
+	 }
+
+	
 	@Then ("^the following fields should appear on the home page \"([^\"]*)\",\"([^\"]*)\",option \"([^\"]*)\"$")
 	 public void signupwithouttypehidden(String emaillable, String passlabel, String type) {
 		assertEquals(emaillable,session().getText("ui=PortalPage::SignupEmailLabel()"));
@@ -68,6 +91,11 @@ public class EmailSignupsteps {
 		session().click("ui=PortalPage::streamlinegetStart()");
 		
 	}
+	
+	 @When("^I click on the green Get Started button for original signup$")
+	 public void ClickonGetStartedjustwebstype() {
+		 session().click("ui=PortalPage::getStart()");
+	 }
 	
 	@Then("^I should remain on the home page$")
 		public void TheHomePageGetStartedGreyout() {
@@ -84,6 +112,7 @@ public class EmailSignupsteps {
 
 	 @When("^I enter in an \"([^\"]*)\"$")
 	 public void EnterEmail(String email) {
+		 this.enteredEmail=email;
 		 session().type("ui=PortalPage::SignupEmailText()", email);
 	 }
 	 
@@ -106,7 +135,42 @@ public class EmailSignupsteps {
 	 @Then("^the fields should be blank$")
         	public void FieldsIsBlank() {
 		 session().waitForPageToLoad("30000");
-		 assertEquals("",session().getText("ui=PortalPage::SignupEmailText()"));
-		 assertEquals("",session().getText("ui=PortalPage::SignupPasswordText()"));
+		 assertEquals(session().getText("ui=PortalPage::SignupEmailText()"),"");
+		 assertEquals(session().getText("ui=PortalPage::SignupPasswordText()"),"");
 	 }
+	 
+	 @Then("^I should be taken to the \"([^\"]*)\" page$")
+	 	public void NavigateToDashboard(String FreeSignuptitle) {
+		 session().waitForPageToLoad("30000");
+		 assertEquals(session().getText("ui=NewSiteSignup::Signuptitle()"),FreeSignuptitle);
+       }
+
+	 @Then("^I should be logged into the email address I just entered$")
+	 public void AssertEmailLoggedInSignupForm() {
+		 assertEquals(enteredEmail+". To use a different WebsID, please log out.", session().getText("//div[@id='step1']/descendant::span[@class='signup_bulletext']"));
+	 }
+
+	 @Then ("^I should see a message \"([^\"]*)\" bubble at the top$")
+	 @Pending
+	 	public void WebsIdCongratsMessage(String Message) {
+	 }
+	 
+	 @When("^I select \"([^\"]*)\" as Website type$")
+	 public void SelectWebsiteType(String webtype) {
+		 session().click("//div[contains(@style,'display: block;')]/form[contains(@id,'signup')]/descendant::div[@class='select_selected']");
+		 session().click("//div[contains(@style,'display: block;')]/form[contains(@id,'signup')]/descendant::ul[@class='select_ul']/li[contains(text(),'"+webtype+"')]");
+	 }
+
+	 @Then("^I should see \"([^\"]*)\" highlighted in Site Category$")
+
+    public void AssertHighlightedSiteCategory(String webtype) {
+		 assertEquals(webtype, session().getText("//div[contains(@id,'site_categories_container')]/descendant::*/a[contains(@class,'selected_purpose')]"));
+	 }
+	 
+	 @Then("^I close the browser instant$")
+	 	public void CloseBrowserInstant() {
+		 session().stop();
+	 }
+
+	 
 }
